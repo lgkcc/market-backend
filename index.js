6d1d15fd9347c56002908e1fcac247bb115bd711
lgkcc <lgkcc@yandex.ru>
2022-08-16 14:17:31 +0700

    add new endpoints and connect mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express'
+import mongoose from "mongoose";
 import cors from "cors";
-const app = express()
 
+mongoose.connect('mongodb+srv://market-server:adminadmin@cluster0.0u1lptq.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('db connect')
+    }).catch((err) => console.log('db error', err))
+
+const app = express()
 app.use(express.json())
 app.use(cors())
-
 app.get('/', (req, res) => res.json('Server is worked'))
+app.get('/category', (req, res) => res.json('Server is worked'))
+app.get('/products', (req, res) => res.json('Server is worked'))
+app.get('/products/:categoryname', (req, res) => res.json('Server is worked'))
+app.get('/products/:id', (req, res) => res.json('Server is worked'))
 
 app.listen(4444, err => {
     err ? console.log(err) : console.log('server start')
